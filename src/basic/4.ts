32dc7d72bd802adca033a403b20240f9ae4997b2
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-04-11 11:24:54 +0200

    Add tasks to files

---
+++ src/basic/4.ts
@@ -0,0 +1,13 @@
+function showMessage(message) {
+  console.log(message);
+}
+
+function calc(num1, num2) {
+  return num1 + num2;
+}
+
+function customError() {
+  throw new Error("Error");
+}
+
+/* Як ви вкажете типи для аргументів і значень цих функцій, що повертаються? */
