909072667844d2bf0b0f281fe504616edfccd04d
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-04-13 12:44:04 +0200

    Update files in generics folder

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,8 +1,6 @@
-/* Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними. Використовуйте Generics для типізації повернутих даних. */
-
 import axios from "axios";
 
-async function fetchData(url) {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,8 +1,3 @@
-/* У вас є тип AllType. 
-Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
-Ваше завдання – використовувати Pick та generics для вказівки, що поля цих параметрів належать AllType. 
-Функція compare повинна повертати AllType. */
-
 type AllType = {
   name: string;
   position: number;
@@ -10,7 +5,10 @@ type AllType = {
   weight: number;
 };
 
-function compare(top, bottom): AllType {
+type Top = Pick<AllType, "name" | "color">;
+type Bottom = Pick<AllType, "position" | "weight">;
+
+function compare<T extends Top, U extends Bottom>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
